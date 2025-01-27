ca27320ae2fb52d022fc527838bfc525669f85f1
Sangram Thapa <thapasangram64@gmail.com>
2025-01-27 19:57:35 +0545

    carrer section top added

--- src/Components/Pages/Career/Career.jsx
+++ src/Components/Pages/Career/Career.jsx
@@ -1,5 +1,11 @@
 function Career() {
-  return <div>Career</div>;
+  return (
+    <div className="bg-gray-400 text-white p-20 text-3xl">
+      <div className="flex justify-center align-center max-w-6xl mx-auto">
+        Career
+      </div>
+    </div>
+  );
 }
 
 export default Career;

b38e30494808d34edff2ab5f39b81ae47ffc585e
Sangram Thapa <thapasangram64@gmail.com>
2025-01-28 13:14:52 +0545

    helo today

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -3,7 +3,7 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { useState } from "react";
 function Navigation() {
   const [showMediaIcons, setShowMediaIcons] = useState(false);
-  const [isClicked, setIsClicked] = useState(false);
+
   return (
     <div className="shadow-md ">
       <div className="grid grid-cols-2 max-w-6xl mx-auto bg-white py-1 px-2 it items-center ">
@@ -13,55 +13,79 @@ function Navigation() {
         <div
           className={
             showMediaIcons
-              ? " flex flex-wrap justify-center align-center h-10 bg-green top-15 fixed bg-blue-400"
+              ? " flex flex-wrap justify-center align-center h-10  top-14 fixed w-full"
               : "hidden lg:flex justify-end"
           }
         >
-          <ul
-            className={
-              isClicked
-                ? "flex items-center gap-5 text-blue-500"
-                : "flex items-center text-gray-700 gap-5 "
-            }
-          >
+          <ul className="flex w-full justify-end flex-col bg-white p-10 lg:p-2 lg:flex-row items-center text-gray-700 gap-5 ">
             <li>
-              <NavLink to="/home" onClick={() => setIsClicked(!isClicked)}>
+              <NavLink
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "text-amber-400" : ""
+                }
+                to="/home"
+              >
                 <a href="#">Home</a>
               </NavLink>
             </li>
             <li>
-              <NavLink to="/about">
+              <NavLink
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "text-amber-400" : ""
+                }
+                to="/about"
+              >
                 <a href="#">About</a>
               </NavLink>
             </li>
             <li>
-              <NavLink to="/service">
+              <NavLink
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "text-amber-400" : ""
+                }
+                to="/service"
+              >
                 <a href="#">Our Service</a>
               </NavLink>
             </li>
             <li>
-              <NavLink to="/works">
+              <NavLink
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "text-amber-400" : ""
+                }
+                to="/works"
+              >
                 <a href="#">Our works</a>
               </NavLink>
             </li>
             <li>
-              <NavLink to="/career">
+              <NavLink
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "text-amber-400" : ""
+                }
+                to="/career"
+              >
                 <a href="#">Career</a>
               </NavLink>
             </li>
             <li>
-              <NavLink to="/contact">
+              <NavLink
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "text-amber-400" : ""
+                }
+                to="/contact"
+              >
                 <a href="#">Contact Us</a>
               </NavLink>
             </li>
           </ul>
         </div>
-        <div
-          className="flex justify-end"
-          onClick={() => setShowMediaIcons(!showMediaIcons)}
-        >
+        <div className="flex justify-end">
           <a href="#">
-            <GiHamburgerMenu className="grid justify-end lg:hidden" />
+            <GiHamburgerMenu
+              className="grid justify-end lg:hidden"
+              onClick={() => setShowMediaIcons(!showMediaIcons)}
+            />
           </a>
         </div>
       </div>

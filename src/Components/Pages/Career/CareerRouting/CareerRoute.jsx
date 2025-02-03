1f7bef28d494d4b801b88fe796b56ba999833c0e
Sangram Thapa <thapasangram64@gmail.com>
2025-02-03 12:17:32 +0545

    career section completed

--- src/Components/Pages/Career/CareerRouting/CareerRoute.jsx
+++ src/Components/Pages/Career/CareerRouting/CareerRoute.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import { BiSolidShoppingBagAlt } from "react-icons/bi";
 import { TiGroup } from "react-icons/ti";
 import { SlCalender } from "react-icons/sl";
 import { FaLocationDot } from "react-icons/fa6";
-
+import { IoIosSend } from "react-icons/io";
 function CareerRoute() {
   const params = useParams();
   const [careers, setCareers] = useState([]);
@@ -32,17 +32,18 @@ function CareerRoute() {
       <div>
         {filterdata && (
           <>
-            <div className="bg-gray-400 text-white p-20 text-3xl  mb-10">
+            <div className="bg-gray-400 text-white p-20 text-3xl  mb-20">
               <div className="flex justify-center align-center max-w-6xl mt-10 mx-auto">
                 {filterdata.title}
               </div>
             </div>
-            <div className="grid grid-cols-1 lg:grid-cols-2  max-w-6xl mx-auto gap-30 mb-10">
+
+            <div className="grid grid-cols-1 lg:grid-cols-2  max-w-6xl mx-auto gap-5 lg:gap-20 mb-10 p-10 lg:p-0">
               <div
                 dangerouslySetInnerHTML={{ __html: filterdata.excerpt }}
-                className="flex flex-wrap"
+                className="flex flex-wrap "
               />
-              <div>
+              <div className="">
                 <ul className="bg-red-500 text-white p-5">
                   <li className="flex">
                     <div className="p-1">
@@ -69,6 +70,50 @@ function CareerRoute() {
                     <div>Location: {filterdata.location}</div>
                   </li>
                 </ul>
+              </div>
+            </div>
+
+            <div className="grid grid-cols-1 lg:grid-cols-2 max-w-6xl mx-auto gap-4 lg:gap-30 p-10 lg:p-0 mb-20">
+              <div className="flex flex-col gap-2">
+                <div>
+                  <h2>
+                    <b>Responsibilites </b>
+                  </h2>
+                </div>
+
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: filterdata.responsibilites,
+                  }}
+                />
+                <div>
+                  <NavLink to={"/contact"}>
+                    <button className="bg-orange-400 text-white p-2 rounded-lg">
+                      <div className="flex">
+                        <div>Apply Now</div>
+                        <div className="flex p-1.5">
+                          <IoIosSend />
+                        </div>
+                      </div>
+                    </button>
+                  </NavLink>
+                </div>
+              </div>
+              <div className="bg-yellow-300 p-7">
+                <div className="flex flex-col gap-2">
+                  <div>
+                    <h2>
+                      <b>Requirements </b>
+                    </h2>
+                  </div>
+                  <div>
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: filterdata.requirements,
+                      }}
+                    />
+                  </div>
+                </div>
               </div>
             </div>
           </>
